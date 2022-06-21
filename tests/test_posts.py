6f1261560f07ac65fef1553290964238629f202c
Kirill Filipskiy <k.filipskiy15@gmail.com>
2022-06-21 12:22:11 +0300

    refactor: code cleanup

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -7,17 +7,17 @@ post_schema = "core/files/post_schema.json"
 
 
 def test_get_posts_list(posts_list):
-    assert_that(posts_list.status_code).is_equal_to(requests.codes.ok)
-    assert_that(posts_list.json()).is_not_empty()
 
-    for post in posts_list.json():
+    assert_that(posts_list.status_code).is_equal_to(requests.codes.ok)
+    assert_that(posts_list.body).is_not_empty()
+    for post in posts_list.body:
         validate(post_schema, post)
 
 
 def test_get_post(post):
     assert_that(post.status_code).is_equal_to(requests.codes.ok)
 
-    validate(post_schema, post.json())
+    validate(post_schema, post.body)
 
 
 def test_create_post(new_post):
@@ -25,10 +25,10 @@ def test_create_post(new_post):
     but it will be faked as if."""
 
     assert_that(new_post.status_code).is_equal_to(requests.codes.created)
-    assert_that(new_post.json()).contains("body").contains_value("bar1")
-    assert_that(new_post.json()).contains("userId").contains_value(1)
+    assert_that(new_post.body["body"]).is_equal_to("bar1")
+    assert_that(new_post.body["userId"]).is_equal_to(1)
 
-    validate(post_schema, new_post.json())
+    validate(post_schema, new_post.body)
 
 
 def test_update_post(updated_post):
@@ -36,10 +36,10 @@ def test_update_post(updated_post):
     but it will be faked as if."""
 
     assert_that(updated_post.status_code).is_equal_to(requests.codes.ok)
-    assert_that(updated_post.json()).contains("body").contains_value("bar2")
-    assert_that(updated_post.json()).contains("userId").contains_value(1)
+    assert_that(updated_post.body["body"]).is_equal_to("bar2")
+    assert_that(updated_post.body["userId"]).is_equal_to(1)
 
-    validate(post_schema, updated_post.json())
+    validate(post_schema, updated_post.body)
 
 
 def test_patch_post(patched_post):
@@ -47,24 +47,12 @@ def test_patch_post(patched_post):
     but it will be faked as if."""
 
     assert_that(patched_post.status_code).is_equal_to(requests.codes.ok)
-    assert_that(patched_post.json()).contains("title").contains_value("foo123")
-    assert_that(patched_post.json()).contains("userId").contains_value(1)
+    assert_that(patched_post.body["title"]).is_equal_to("foo123")
+    assert_that(patched_post.body["userId"]).is_equal_to(1)
 
-    validate(post_schema, patched_post.json())
+    validate(post_schema, patched_post.body)
 
 
 def test_delete_post(deleted_post):
     """Regarding documentation: resource will not be really updated on the server but it will be faked as if."""
     assert_that(deleted_post.status_code).is_equal_to(requests.codes.ok)
-
-
-# def validate(json_schema, responce_body):
-#     validator = Validator(read_json_file(json_schema), require_all=True)
-
-#     assert validator.validate(responce_body) is True, validator.errors
-
-
-# def read_json_file(file):
-#     with open(file) as data:
-#         json_object = json.load(data)
-#     return json_object
